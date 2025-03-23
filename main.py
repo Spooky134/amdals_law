a23117a2ab3c81faf3663c8d90b3981b0c4db074
andreychvankov <whitebear134@gmail.com>
2025-03-24 00:46:04 +0300

    second commit

--- main.py
+++ main.py
@@ -3,21 +3,28 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    amdl_list = ((AmdalN(1, 100, 1, 0.1), {'x_label': 'n', 'y_label': 'R'}),
-                 (AmdalA(0.1, 0.9, 0.1, 5), {'x_label': 'a', 'y_label': 'R'}),
-                 (AmdalNetworkN(1, 80, 1, 0.1, 0.1, 0.1), {'x_label': 'n', 'y_label': 'R'}),
-                 (AmdalNetworkA(1, 80, 1, 5, 0.1, 0.1), {'x_label': 'a', 'y_label': 'R'}),
-                 (AmdalNetworkCa(0.1, 0.9, 0.1, 5, 0.1, 0.1), {'x_label': 'ca', 'y_label': 'R'}),
-                 (AmdalNetworkCt(0.1, 0.9, 0.1, 5, 0.1, 0.1), {'x_label': 'ct', 'y_label': 'R'}),)
+    amdl_list = (AmdalN(1, 100, 1, 0.1),
+                 AmdalA(0.1, 0.9, 0.1, 5),
+                 AmdalNetworkN(1, 80, 1, 0.1, 0.1, 0.1),
+                 AmdalNetworkA(1, 80, 1, 5, 0.1, 0.1),
+                 AmdalNetworkCa(0.1, 0.9, 0.1, 5, 0.1, 0.1),
+                 AmdalNetworkCt(0.1, 0.9, 0.1, 5, 0.1, 0.1))
     
-    for i, amdl in enumerate(amdl_list):
-        res = amdl[0].evalute()
+    labels = ({'x_label': 'n', 'y_label': 'R'},
+              {'x_label': 'a', 'y_label': 'R'},
+              {'x_label': 'n', 'y_label': 'R'},
+              {'x_label': 'a', 'y_label': 'R'},
+              {'x_label': 'ca', 'y_label': 'R'},
+              {'x_label': 'ct', 'y_label': 'R'})
+    
+    for i, amdl, label in zip(range(1, len(labels)+1), amdl_list, labels):
+        res = amdl.evalute()
 
         plt.figure(i, figsize=(8, 4))
-        plt.plot([el.x for el in res], [el.y for el in res], label=amdl[0].FORMULA)
-        plt.title(f'Graph {amdl[0].NAME}')
-        plt.xlabel(amdl[1]["x_label"])
-        plt.ylabel(amdl[1]["y_label"]) 
+        plt.plot([point.x for point in res], [point.y for point in res], label=amdl.FORMULA)
+        plt.title(f'Graph {amdl.NAME}')
+        plt.xlabel(label["x_label"])
+        plt.ylabel(label["y_label"]) 
         plt.grid(True)
         plt.legend()
 
